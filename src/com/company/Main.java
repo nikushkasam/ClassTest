f6f375fe0a482d2beb33bf88d66cd585e0162c7f
UITSCHOOL <test@mail.com>
2020-03-07 12:17:11 +0300

    Commit new master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int a = 10;
         System.out.println("Hi");
         System.out.println("Hi");
         System.out.println("Hi");
